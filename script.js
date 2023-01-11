02deed5dde7207d04572796c5b51165a9247902f
Dongja <jdh981118@naver.com>
2023-01-11 13:50:05 +0900

    dblclick시 수정

--- script.js
+++ script.js
@@ -63,18 +63,7 @@ modal_register_btn.addEventListener('click',(e)=>{
 //add에서 input값 받는 함수
 const onChange = (e)=>{
     input_data[e.target.name] = e.target.value;
-    const register_button = document.querySelector('.register-button');
-    const input_items = document.querySelector('.input-items');
-    if(!register_button)
-        return;
-    const register_status = input_data['title'] && input_data['contents'];
-    if(register_status){
-        register_button.disabled = false;
-        input_items.style.opacity = 1;
-        return;
-    }
-    register_button.disabled = true;
-    input_items.style.opacity = 0.4;
+    modifyRegisterButton();
 }
 
 const changeNotificationMode = ()=>{
@@ -87,41 +76,72 @@ document.querySelector('.fa-bars').addEventListener('click',changeNotificationMo
 document.querySelector('.delete-notification-menu').addEventListener('click',changeNotificationMode);
 
 const autoResizeTextarea = (e) => {
-    // console.log(e);
     e.target.style.height ='auto';
     e.target.style.height = `${e.target.scrollHeight}px`
-    // console.log(e);
 };
 
 const addInputEvent = ()=>{
     //input창 이벤트
-    document.querySelector('.input-title').focus();
-    document.querySelector('.input-title').addEventListener('change',onChange);
-    document.querySelector('.input-contents').addEventListener('change',onChange);
-    document.querySelector('.input-title').addEventListener('keydown',autoResizeTextarea,true);
-    document.querySelector('.input-contents').addEventListener('keydown',autoResizeTextarea,true);
+
+    const input_title = document.querySelector('.input-title');
+    const input_contents = document.querySelector('.input-contents');
+
+    input_title.focus();
+    input_title.addEventListener('change',onChange);
+    input_contents.addEventListener('change',onChange);
+    input_title.addEventListener('keydown',autoResizeTextarea,true);
+    input_contents.addEventListener('keydown',autoResizeTextarea,true);
+    modifyRegisterButton();
 };
 
+const checkBeforeUpdateItem = ()=>{
+    return BeforeUpdateItem.Title && BeforeUpdateItem.Contents && BeforeUpdateItem.Status;
+}
+
+/**
+ * if before data가 있으면 컨텐츠 변경하고
+ *  html 바꿈
+ * 그렇지 않으면 원래대로 맨위로 추가
+ * 
+ */
 const addInputRegisterEvent = (e)=>{
     //등록버튼 이벤트
     document.querySelector('.input-items').querySelector('.register-button').addEventListener('mousedown',()=>{
+        if(checkBeforeUpdateItem()){
+            const index = todos.findIndex(e => e.Title === BeforeUpdateItem.Title && e.Contents === BeforeUpdateItem.Contents && e.Status === BeforeUpdateItem.Status);
+            todos[index] = {...todos[index], 
+                Title:input_data['title'],
+                Contents:input_data['contents']}
+            console.log(todos[index]);
+            initializeToBeUpdatedItem();
+            initializeInputData();
+            render();
+            return;
+        }
+        
+        const input_status = document.querySelector('.input-items').closest('section').className;
         todos.unshift({
-            Status:e.target.closest('section').className,
+            Status:input_status,
             Title:input_data['title'],
             Contents:input_data['contents']});
-        input_data['title'] ='';
-        input_data['contents'] = '';
+        initializeToBeUpdatedItem();
+        initializeInputData();
         document.querySelector('.input-items').remove();
         render();
     });
 };
 
+/**
+ * if before data가 있으면
+ *  html 바꿈
+ * 그렇지 않으면 원래대로 제거
+ */
 const addInputFocusOutEvent = ()=>{
     //focusout 이벤트
     document.querySelector('.input-items').addEventListener("blur", ()=>{
         setTimeout(()=>{
-            input_data['title'] ='';
-            input_data['contents'] = '';
+            initializeToBeUpdatedItem();
+            initializeInputData();
             const input_item = document.querySelector('.input-items');
             if(input_item)
             input_item.remove();
@@ -129,42 +149,62 @@ const addInputFocusOutEvent = ()=>{
     })
 }
 
+/**
+ * if before data가 있으면
+ *  html 바꿈
+ * 그렇지 않으면 원래대로 제거
+ */
 const addInputCancelEvent = ()=>{
     //취소버튼 이벤트
     document.querySelector('.input-items').querySelector('.cancel-button').addEventListener('click',(e)=>{
-        input_data['title'] ='';
-        input_data['contents']  = '';
+        initializeToBeUpdatedItem();
+        initializeInputData();
         document.querySelector('.input-items').remove();});
 }
 
-const make_new_input = ()=>{
+const make_new_input = (mode)=>{
     const newInput =
     `<li class = 'input-items' , tabindex = -1>
         <form>
-            <textarea class = 'input-title' type = 'text' placeholder = '제목을 입력하세요' maxlength = 500 name = 'title' 'onchange' = 'onChange()'></textarea>
-            <textarea class = 'input-contents' type = 'text' placeholder = '내용을 입력하세요' maxlength = 500 name = 'contents' 'onchange' = onChange'></textarea>
+            <textarea class = 'input-title' type = 'text' placeholder = '제목을 입력하세요' maxlength = 500 name = 'title' 'onchange' = 'onChange()'>${input_data['title']? input_data['title'] : ''}</textarea>
+            <textarea class = 'input-contents' type = 'text' placeholder = '내용을 입력하세요' maxlength = 500 name = 'contents' 'onchange' = onChange'>${input_data['contents'] ? input_data['contents']  : ''}</textarea>
             <div class = 'buttons'>
                 <button type ='button' class = 'cancel-button'>취소</>
-                <button type ='button' class = 'register-button' disabled>등록</>
+                <button type ='button' class = 'register-button' disabled>${mode}</>
             </div>
         </form>
     </li>`
     return newInput;
 }
 
+const checkInputStatus = () => {
+    return input_data['title'] && input_data['contents'];
+}
+
+const modifyRegisterButton = () => {
+    const register_button = document.querySelector('.register-button');
+    const input_items = document.querySelector('.input-items');
+    if(checkInputStatus(register_button)){
+        register_button.disabled = false;
+        input_items.style.opacity = 1;
+        return;
+    }
+    register_button.disabled = true;
+    input_items.style.opacity = 0.4;
+}
 
 //이벤트 한꺼번에 설정해 위임
 const todolist = document.querySelector('.todolist');
 todolist.addEventListener('click',(e)=>{
     //add 이벤트
-    if(e.target.className === 'add' || e.target.classList[1] === "fa-plus"){
+    if(e.target.classList[1] === "fa-plus"){
         const input_items = document.querySelector('.input-items');
         if(input_items){
             input_items.remove();
             return;
         }
         const section = e.target.closest('section').querySelector('ul');
-        const input_new_element = make_new_input();
+        const input_new_element = make_new_input('등록');
         section.insertAdjacentHTML('afterbegin',input_new_element);
 
         addInputEvent();
@@ -176,11 +216,80 @@ todolist.addEventListener('click',(e)=>{
 
     //delete 이벤트
     const id = e.target.dataset.id;
-    if(!e.target.dataset.id || e.target.tagName !== 'I')
-        return;
-    storeDeletedItem(id);
-    modal.classList.toggle('act');
+    if(e.target.dataset.id && e.target.tagName === 'I'){
+        storeDeletedItem(id);
+        modal.classList.toggle('act');
+    }
 })
+
+const BeforeUpdateItem = {
+    Status : null,
+    Title : null,
+    Contents: null
+}
+
+/**
+     * dbclick 이벤트
+     * 1. 리스트 아이템 클릭하면 인풋창으로 변경 -> 이전 값들 BeforeUpdate, input_data에 저장
+     * 2. 취소 누르면 수정 취소
+     * 3. 등록 누르면 반영 => 배열에 이전 값들을 갖고 있는 객체 뒤져서 있는지 확인
+     * 4. 
+     */
+todolist.addEventListener('dblclick',(e)=>{
+    console.log(e.target);
+    const update_element = e.target.closest('.todolist-items');
+    if(!update_element)
+        return;
+    console.log('update_element',update_element);
+    storeToBeUpdatedItem(update_element);
+    storeInputData();
+    console.log('Before data',BeforeUpdateItem);
+
+    update_element.className = 'input-items';
+    // update_element.setattribute('tabindex', -1);
+
+    update_element.innerHTML = 
+    `<form>
+        <textarea class = 'input-title' type = 'text' placeholder = '제목을 입력하세요' maxlength = 500 name = 'title' 'onchange' = 'onChange()'>${BeforeUpdateItem.Title ? BeforeUpdateItem.Title : ''}</textarea>
+        <textarea class = 'input-contents' type = 'text' placeholder = '내용을 입력하세요' maxlength = 500 name = 'contents' 'onchange' = onChange'>${BeforeUpdateItem.Contents ? BeforeUpdateItem.Contents : ''}</textarea>
+        <div class = 'buttons'>
+            <button type ='button' class = 'cancel-button'>취소</>
+            <button type ='button' class = 'register-button' disabled>수정</>
+        </div>
+    </form>
+    `;
+
+    addInputEvent();
+    addInputCancelEvent();
+    addInputRegisterEvent(e);
+    addInputFocusOutEvent();
+    modifyRegisterButton();
+
+    // initializeToBeUpdatedItem();
+    // initializeInputData();
+})
+
+const initializeToBeUpdatedItem = () =>{
+    BeforeUpdateItem.Status = null;
+    BeforeUpdateItem.Title = null;
+    BeforeUpdateItem.Contents = null;
+}
+
+const initializeInputData = () => {
+    input_data['title'] ='';
+    input_data['contents'] = '';
+}
+
+const storeToBeUpdatedItem = (update_element)=>{
+    BeforeUpdateItem.Status = update_element.closest('section').className;
+    BeforeUpdateItem.Title = update_element.querySelector('h3').innerText;
+    BeforeUpdateItem.Contents = update_element.querySelector('p').innerText;
+}
+
+const storeInputData = ()=>{
+    input_data['title'] = BeforeUpdateItem.Title;
+    input_data['contents'] = BeforeUpdateItem.Contents;
+}
 
 const storeDeletedItem = (id)=>{
     const toBedeleted = document.querySelector(`.todolist-items[data-id = "${id}"]`);
