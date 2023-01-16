7270f5346b006834d4230a14015af460c281a3e1
Dongja <jdh981118@naver.com>
2023-01-16 10:31:04 +0900

    mainjs파일 이름 변경

--- dataProcessing.js
+++ dataProcessing.js
@@ -1,32 +1,40 @@
-import {BeforeUpdateItem, InputData, ToBeDeleted} from "./store.js";
+import { BeforeUpdateItem, InputData, ToBeDeleted } from "./store.js";
 
-const initializeToBeUpdatedItem = () =>{
-    BeforeUpdateItem.Status = '';
-    BeforeUpdateItem.Title = '';
-    BeforeUpdateItem.Contents = '';
-}
+const initializeToBeUpdatedItem = () => {
+  BeforeUpdateItem.Status = "";
+  BeforeUpdateItem.Title = "";
+  BeforeUpdateItem.Contents = "";
+};
 
 const initializeInputData = () => {
-    InputData['title'] ='';
-    InputData['contents'] = '';
-}
+  InputData["title"] = "";
+  InputData["contents"] = "";
+};
 
-const storeToBeUpdatedItem = (update_element)=>{
-    BeforeUpdateItem.Status = update_element.closest('section').className;
-    BeforeUpdateItem.Title = update_element.querySelector('h3').innerText;
-    BeforeUpdateItem.Contents = update_element.querySelector('p').innerText;
-}
+const storeToBeUpdatedItem = (update_element) => {
+  BeforeUpdateItem.Status = update_element.closest("section").className;
+  BeforeUpdateItem.Title = update_element.querySelector("h3").innerText;
+  BeforeUpdateItem.Contents = update_element.querySelector("p").innerText;
+};
 
-const storeInputData = ()=>{
-    InputData['title'] = BeforeUpdateItem.Title;
-    InputData['contents'] = BeforeUpdateItem.Contents;
-}
+const storeInputData = () => {
+  InputData["title"] = BeforeUpdateItem.Title;
+  InputData["contents"] = BeforeUpdateItem.Contents;
+};
 
-const storeDeletedItem = (id)=>{
-    const toBedeleted = document.querySelector(`.todolist-items[data-id = "${id}"]`);
-    ToBeDeleted.Status = toBedeleted.closest('section').className;
-    ToBeDeleted.Title = toBedeleted.querySelector('h3').innerText;
-    ToBeDeleted.Contents = toBedeleted.querySelector('p').innerText;
-}
+const storeDeletedItem = (id) => {
+  const toBedeleted = document.querySelector(
+    `.todolist-items[data-id = "${id}"]`
+  );
+  ToBeDeleted.Status = toBedeleted.closest("section").className;
+  ToBeDeleted.Title = toBedeleted.querySelector("h3").innerText;
+  ToBeDeleted.Contents = toBedeleted.querySelector("p").innerText;
+};
 
-export {initializeToBeUpdatedItem, initializeInputData, storeToBeUpdatedItem, storeInputData, storeDeletedItem} ;
+export {
+  initializeToBeUpdatedItem,
+  initializeInputData,
+  storeToBeUpdatedItem,
+  storeInputData,
+  storeDeletedItem,
+};
